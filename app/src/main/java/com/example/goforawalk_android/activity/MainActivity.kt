cf21ad94c7eaebdbcc2570a1d9b946a316bed1cc
yujung <yujung7439@gmail.com>
2025-04-10 14:42:46 +0900

    [Feature1] 로그인 UI구현

---
+++ app/src/main/java/com/example/goforawalk_android/activity/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.goforawalk_android.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.goforawalk_android.ui.screen.LoginScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoginScreen()
+        }
+    }
+}

9f2b559672c48e8bfa86e01ce178a7d1ca7e6a0d
yujung <yujung7439@gmail.com>
2026-01-08 22:40:14 +0900

    [Feature16] 캘린더 바텀 네비게이션 추가, 인증기능은 FloatingActionButton으로 이동

---
+++ app/src/main/java/com/yjdev/goforawalk/presentation/ui/screen/CalendarScreen.kt
@@ -0,0 +1,8 @@
+package com.yjdev.goforawalk.presentation.ui.screen
+
+import androidx.compose.runtime.Composable
+import com.yjdev.goforawalk.presentation.viewmodel.MainViewModel
+
+@Composable
+fun CalendarScreen(viewModel: MainViewModel) {
+}
